d7fc5574a178c3c8a3db2c5ae2264b8e58dc3c99
Anton_Chernyshev <playhardcoreordie@gmail.com>
2023-04-26 17:05:34 +0300

    task04

---
+++ Task_04/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задайте одномерный массив из 10 случайных чисел. Найдите количество элементов массива, значения которых лежат в отрезке [10,99]
+
+void Task4()
+{
+    int size = 12;
+    int[] array = new int[size];
+    FillArray(array);
+    PrintArray(array);
+    int count = CountElementInSegment(array);
+    Console.WriteLine($"Количество элементов из откезка [10, 99] в массиве равно {count}");
+}
+Task4();
+
+void FillArray(int[] array, int minValue = -100, int maxValue = 100)
+{
+    maxValue++;
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(minValue, maxValue);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("Задан массив: ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int CountElementInSegment(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] >= 10 && array[i] <= 99)
+        {
+            count++;
+        }
+    }
+    return count;
+}
